d4f930a4064cad6b66d4c58247df7af099fffe1e
drobushevskiy <a.drobushevskiy@okkapi-art.ru>
2018-12-30 21:16:15 +0300

    first commit

---
+++ resources/views/threads/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="#">{{ $thread->creator->name }}</a> posted:
+                    {{ $thread->title }}
+                </div>
+
+                <div class="panel-body">
+                    {{ $thread->body }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+
+                @foreach ($thread->replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+
+        </div>
+    </div>
+
+    @if (auth()->check())
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                hello
+            </div>
+        </div>
+    @endif
+
+</div>
+@endsection
